98a072446d47a4b50737b23093b63e47d4794eda
Pranav Ghorpade <pranavghorpade61@gmail.com>
2025-08-09 22:59:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Length of The Array/length-of-the-array.java
@@ -0,0 +1,15 @@
+class Solution {
+    public static int arrayLength(int[] arr) {
+        // code here
+        return arr.length;
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner (System.in);
+        int n = sc.nextInt();
+        int arr [] = new int[n];
+        for(int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.print(arrayLength(arr));
+    }
+}
