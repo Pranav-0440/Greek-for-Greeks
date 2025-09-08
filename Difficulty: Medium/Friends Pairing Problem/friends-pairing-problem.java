024b2a365ef66303cb5d114e9cf006374c1847f0
Pranav Ghorpade <pranavghorpade61@gmail.com>
2025-09-08 22:50:24 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Friends Pairing Problem/friends-pairing-problem.java
@@ -0,0 +1,19 @@
+// User function Template for Java
+
+class Solution {
+    public long countFriendsPairings(int n) {
+        // code here
+        if(n == 1 || n == 2){
+            return n;
+        }
+        
+        long fnm1 = countFriendsPairings(n-1);
+        long fnm2 = countFriendsPairings(n-2);
+        
+        long pairways = (n-1) * fnm2;
+        
+        long totalways = fnm1 + pairways;
+        
+        return totalways;
+    }
+}
